5591edf094833ec1f6a6acf8fb59b51b738ae26a
eadmaster <925171+eadmaster@users.noreply.github.com>
2026-04-03 04:00:08 +0200

    popful mail: added missing lines

--- examples/Popful Mail PCECD/_build_fixed.sh
+++ examples/Popful Mail PCECD/_build_fixed.sh
@@ -15,13 +15,24 @@ repeat_block() {
 
 patch_repeated_blocks() {
     repeat_block 0x22e782  0xf3e6a3  1128  "$1"  # items in submenu 
+
+    #repeat_block 0x2851f0 0x2351b6   72  "$OUTPUT_ROM"  # shop menu1
+    #repeat_block 0x2852ae 0x235274 2382  "$OUTPUT_ROM"  # shop items
+    #repeat_block 0x29d019 0x24d019  520  "$OUTPUT_ROM"  # shop menu2
+    #repeat_block 0x43d019 0x3ed019  463  "$OUTPUT_ROM"  # shop menu3 -> TODO: test
+    # variations: 
+    #repeat_block 0x8351e2 0x2351b6   72  "$OUTPUT_ROM"  # shop menu1 (alt) -> TODO: test
+    #repeat_block 0x63d019 0x24d019  520  "$OUTPUT_ROM"  # shop menu2 (alt) -> TODO: test
+    #repeat_block 0x6dd019 0x24d019  520  "$OUTPUT_ROM"  # shop menu2 (alt) -> TODO: test
+    #repeat_block 0x821098 0x235274 2380  "$OUTPUT_ROM"  # shop items (alt. smaller) -> TODO: test
+    #repeat_block 0x8352a0 0x235274 2380  "$OUTPUT_ROM"  # shop items (alt. smaller) -> TODO: test
 }
     
 
 extract_gfx() {
     mkdir gfx
-    sfk partcopy "$OUTPUT_ROM" -fromto 0x1060860 0x10616c0 gfx/locations_jap.bin -yes
-    #TODO: sfk partcopy "$OUTPUT_ROM" -fromto ... gfx/menu_jap.bin -yes
+    #sfk partcopy "$OUTPUT_ROM" -fromto 0x1060860 0x10616c0 gfx/_locations_jap.bin -yes
+    #sfk partcopy "$OUTPUT_ROM" -fromto 0x251D50 0x2571f0 gfx/_pause_menu_jap.bin -yes
 }
 
 patch_gfx() {
